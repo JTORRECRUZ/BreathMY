db54d958707cd0cc57b6cd691daa559c8f6746e0
aasn0001 <74348298+aasn0001@users.noreply.github.com>
2024-04-04 12:11:31 +0200

    Update spectrogram.m

--- Implementation/spectrogram.m
+++ Implementation/spectrogram.m
@@ -2,7 +2,7 @@
 %++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 %%                               Spectrogram
 %++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-%   Authors:   Francisco Jesus Canadas Quesada Juan de la Torre Cruz
+%   Authors:   Francisco Jesus Canadas Quesada 
 %              Juan de la Torre Cruz
 %              Alejandro Antonio Salvador Navarro
 %
