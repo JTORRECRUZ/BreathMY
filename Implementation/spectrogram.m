46bbbe3526739b6b8043c7932e5310c331f4e2a6
aasn0001 <74348298+aasn0001@users.noreply.github.com>
2024-04-04 12:36:24 +0200

    Update spectrogram.m

--- Implementation/spectrogram.m
+++ Implementation/spectrogram.m
@@ -2,7 +2,7 @@
 %++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 %%                               Spectrogram
 %++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-%   Authors:   Francisco Jesus Canadas Quesada 
+%   Authors:   Francisco Jesus Canadas Quesada
 %              Juan de la Torre Cruz
 %              Alejandro Antonio Salvador Navarro
 %
@@ -23,23 +23,26 @@ function Y = spectrogram(y,nfft,fs,window,noverlap)
 
 nx = length(y); 
 nwind = length(window); 
+
 if nx < nwind % Zero-pad x if it has length less than the window length
     y(nwind)=0;  nx=nwind;
 end
+
 y = y(:); 
 window = window(:);
 
 ncol = fix((nx-noverlap)/(nwind-noverlap)); 
 colindex = 1 + (0:(ncol-1))*(nwind-noverlap); 
 rowindex = (1:nwind)';
-if length(y)<(nwind+colindex(ncol)-1)
-    y(nwind+colindex(ncol)-1) = 0;   
-end
+
+if length(y)<(nwind+colindex(ncol)-1), y(nwind+colindex(ncol)-1) = 0; end
+
 w = zeros(nwind,ncol); 
 w(:) = y(rowindex(:,ones(1,ncol))+colindex(ones(nwind,1),:)-1); % Populate output matrix using sliding window approach
 w = window(:,ones(1,ncol)).*w; 
 
 w = fft(w,nfft); 
+
 if ~any(any(imag(y)))    
     if rem(nfft,2), % Check if nfft is odd
         select = [1:(nfft+1)/2]; 
@@ -50,20 +53,7 @@ if ~any(any(imag(y)))
 else
     select = 1:nfft; % Select all frequencies if x has imaginary part
 end
-f = (select - 1)'*fs/nfft; 
-t = (colindex-1)'/fs; 
-if nargout == 1,
-    Y = w; 
-elseif nargout == 2,
-    Y = w; 
-    fo = f; 
-elseif nargout == 3,
-    Y = w; 
-    fo = f; 
-    to = t; 
-else
-    Y = w; 
-    fo = f; 
-    to = t; 
-    colindex=colindex; 
+
+Y = w; 
+
 end
